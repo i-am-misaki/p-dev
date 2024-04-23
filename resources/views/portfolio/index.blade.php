0e7e0c76083a7039a13d496ec9bce60a8bd1d3dd
i-am-misaki <mkmtnm@yahoo.com>
2024-04-23 22:59:15 +0900

    css終わり

--- resources/views/portfolio/index.blade.php
+++ resources/views/portfolio/index.blade.php
@@ -18,17 +18,28 @@
        <div class="my-20">
            <h2 class="flex justify-center font-normal text-3xl">新規登録</h2>
        </div>
-       <div class="flex flex-col justify-center w-96 h-82 mb-80">
-           <label>氏名</label>
-           <input class="border-none outline-none border-b border-slate-500"></input>
-           <label>メールアドレス</label>
-           <input class="border-none outline-none border-b border-slate-500"></input>
-           <label>パスワード</label>
-           <input class="border-none outline-none border-5 border-b-indigo-500"></input>
-           <div class="text-center">
-               <x-element.button-a :href="route('register')">登録する</x-element.button-a>
+       <form >
+           <div class="flex flex-col justify-center w-96 h-82 mb-80">
+               <label name='name'>氏名</label>
+               @error('name')
+               <p style="color: red;">{{ $message }}</p>
+               @enderror
+               <input class="mb-8 border-none outline-none border-b border-slate-500"></input>
+               <label name='email'>メールアドレス</label>
+               @error('email')
+               <p style="color: red;">{{ $message }}</p>
+               @enderror
+               <input class="mb-8 border-none outline-none border-b border-slate-500"></input>
+               <label name='pass'>パスワード</label>
+               @error('pass')
+               <p style="color: red;">{{ $message }}</p>
+               @enderror
+               <input class="mb-8 border-none outline-none border-5 border-b-indigo-500"></input>
+               <div class="text-center">
+                   <x-element.button-a :href="route('register')">登録する</x-element.button-a>
+               </div>
            </div>
-       </div>
+       </form>
     </div>
     <footer>
         <h4 class="text-white w-full bg-cyan-800 flex justify-center">portfolio site</h4>
