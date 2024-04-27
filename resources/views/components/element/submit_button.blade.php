011f22787d82965291dec7a26a6ad379df1b5e19
i-am-misaki <mkmtnm@yahoo.com>
2024-04-27 20:54:02 +0900

    +display

--- resources/views/components/element/submit_button.blade.php
+++ resources/views/components/element/submit_button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'bg-cyan-800 text-white py-2 px-14 rounded-sm']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'bg-cyan-800 text-white display: inline-block py-2 px-16 rounded-sm']) }}>
     {{ $slot }}
 </button>
