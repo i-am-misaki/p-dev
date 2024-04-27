5bbf163105148f7e0a78d56888b4cc665e3e014c
i-am-misaki <mkmtnm@yahoo.com>
2024-04-27 20:41:18 +0900

    without t-b padding

--- resources/views/components/element/submit_button.blade.php
+++ resources/views/components/element/submit_button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => '']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'bg-cyan-800 text-white px-14 rounded-sm']) }}>
     {{ $slot }}
 </button>
