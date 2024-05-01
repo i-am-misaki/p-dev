d94f75f76f519ad1bc562811d9679b271b520ec6
i-am-misaki <mkmtnm@yahoo.com>
2024-05-01 23:10:16 +0900

    top page

--- resources/views/components/portfolio/base.blade.php
+++ resources/views/components/portfolio/base.blade.php
@@ -18,7 +18,7 @@
     <body class="font-sans text-gray-900 ">
         {{ $header }}
         <div class="flex justify-center items-center">
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800  overflow-hidden sm:rounded-lg">
+            <div class="w-full dark:bg-gray-800">
                 {{ $slot }}
             </div>
         </div>
--- resources/views/portfolio/mypage.blade.php
+++ resources/views/portfolio/mypage.blade.php
@@ -1,5 +1,19 @@
 <x-portfolio.base>
     <x-portfolio.header></x-portfolio.header>
+    <div class="w-full py-20">
+        <div class="flex justify-center mx-16">
+            <div class="w-1/2 text-center">
+                <image width=200 height=120 class="m-auto h-80 bg-slate-300 rounded-full" />
+                <h4 class="text-normal font-Roboto text-xl text-center">アカデミー花子</h4>
+            </div>
+            <div id="content" class="w-1/2 gap-4 justify-center flex flex-col">
+                <h2 class="align-middle w-36 font-Roboto text-4xl font-bold">自己紹介</h2>
+                <span class="border-b-2 w-64"></span>
+                <p class="align-middle gap-4 font-Roboto font-normal text-lg">自分の自己紹介文章が入ります。自分の自己紹介文章が入ります。自分の自己紹介文章が入ります。自分の自己紹介文章が入ります。自分の自己紹介文章が入ります。</p>
+                <x-portfolio.button class="w-64 h-14 py-4 px-10 align-middle font-normal text-base font-Roboto">{{__('自己紹介を編集する')}}</x-portfolio.button>
+            </div>
+        </div>
+    </div>
     <x-portfolio.footer></x-portfolio.footer>
 </x-portfolio.base>
 
