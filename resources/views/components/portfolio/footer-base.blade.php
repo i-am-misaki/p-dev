72fe9527340e263c034ab2a8a3108ccc74f8a9b6
i-am-misaki <mkmtnm@yahoo.com>
2024-05-03 18:12:01 +0900

    relative absolute

--- resources/views/components/portfolio/footer-base.blade.php
+++ resources/views/components/portfolio/footer-base.blade.php
@@ -1,5 +1,5 @@
 @slot('footer')
-<footer class="absolute bottom-0 mt-auto x-10 w-full h-10 bg-cyan-800 flex items-center justify-center">
+<footer class="absolute bottom-0 x-10 w-full h-10 bg-cyan-800 flex items-center justify-center">
     <h4 class="text-white font-Roboto leading-5 bg-cyan-800 font-normal text-lg">
         portfolio site
     </h4>
