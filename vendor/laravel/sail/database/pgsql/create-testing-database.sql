c41054e10ab19f1411c1672f018306062cd38f08
i-am-misaki <mkmtnm@yahoo.com>
2024-05-16 22:28:51 +0900

    etc

--- vendor/laravel/sail/database/pgsql/create-testing-database.sql
+++ vendor/laravel/sail/database/pgsql/create-testing-database.sql
@@ -1,2 +1,2 @@
-SELECT 'CREATE DATABASE "academy-laravel"'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'academy-laravel')\gexec
+SELECT 'CREATE DATABASE testing'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'testing')\gexec
