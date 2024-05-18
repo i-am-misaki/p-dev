291a63cf389cfa612b5e3632f8e9ea739d783fbf
i-am-misaki <mkmtnm@yahoo.com>
2024-05-18 15:13:21 +0900

    updated

--- resources/views/layouts/skill_base.blade.php
+++ resources/views/layouts/skill_base.blade.php
@@ -15,7 +15,7 @@
 
         
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/make_select.js'])
         <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     </head>
     <body class="font-sans text-gray-900">
